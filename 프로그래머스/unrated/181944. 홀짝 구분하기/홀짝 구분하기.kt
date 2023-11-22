3f148b589d80fa733c6f8932c0409fa7f84020b8
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-23 00:23:36 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 166.27 ms, Memory: 46.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.kt
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val a = readLine()!!.toInt()
     
-    val str = if (a % 2 == 0) "is even" else "is odd"
-    print("$a $str")
+    val str = if (a % 2 == 0) "even" else "odd"
+    print("$a is $str")
 }
