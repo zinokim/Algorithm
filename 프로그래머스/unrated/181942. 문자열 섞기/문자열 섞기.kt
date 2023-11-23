54f5ca192dbdef9fb0a74ea8bae3a3d8bb780111
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-23 23:15:42 +0900

    [level unrated] Title: 문자열 섞기, Time: 14.74 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.kt
@@ -1,5 +1,5 @@
 class Solution {
     fun solution(str1: String, str2: String): String {
-        return str1.zip(str2).joinToString("") { (a, b) -> "$a$b" }
+        return str1.zip(str2).map { "${it.first}${it.second}" }.joinToString("")
     }
 }
