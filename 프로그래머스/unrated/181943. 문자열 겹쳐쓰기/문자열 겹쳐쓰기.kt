90f4914d06bb4c3ebe5550ec96b9a8e7f798c6d4
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-23 22:38:09 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 6.82 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -0,0 +1,7 @@
+import kotlin.math.*
+
+class Solution {
+    fun solution(my_string: String, overwrite_string: String, s: Int): String {
+        return my_string.replaceRange(s, s + overwrite_string.length, overwrite_string)
+    }
+}
