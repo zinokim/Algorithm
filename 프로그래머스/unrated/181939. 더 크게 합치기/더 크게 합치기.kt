d2fa4b815349be9bc146498f332f3d7749e0218e
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-24 23:44:46 +0900

    [level unrated] Title: 더 크게 합치기, Time: 12.69 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.kt
@@ -2,9 +2,6 @@ import kotlin.math.max
 
 class Solution {
     fun solution(a: Int, b: Int): Int {
-        val first = a.toString().plus(b).toInt()
-        val second = b.toString().plus(a).toInt()
-        
-        return max(first, second)
+        return max("$a$b".toInt(), "$b$a".toInt())
     }
 }
