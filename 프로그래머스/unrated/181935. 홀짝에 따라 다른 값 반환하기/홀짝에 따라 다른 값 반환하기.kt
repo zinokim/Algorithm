0757338a44df71332a179d7f5e7510617dd8814b
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-27 22:01:46 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 8.50 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -1,17 +1,6 @@
 class Solution {
     fun solution(n: Int): Int {
-        var answer: Int = 0
-        
-        if (n % 2 == 0) {
-            for (i in n downTo 0 step 2) {
-                answer += i * i
-            }
-        } else {
-            for (i in n downTo 0 step 2) {
-                answer += i
-            }
-        }
-        
-        return answer
+        return if (n % 2 == 0) (n downTo 0 step 2).map { it * it }.sum()
+        else (n downTo 0 step 2).sum()
     }
 }
