9b6ba263ae53c8747480c3a12094dfd6154d122e
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-27 23:37:13 +0900

    [level unrated] Title: 조건 문자열, Time: 11.90 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.kt
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.kt
@@ -1,17 +1,11 @@
 class Solution {
     fun solution(ineq: String, eq: String, n: Int, m: Int): Int {
-        if (eq == "=") {
-            if (ineq == ">") {
-                return if (n >= m) 1 else 0
-            }
-            
-            return if (n <= m) 1 else 0
+        return when ("$ineq$eq".replace("!", "")) {
+            ">=" -> if (n >= m) 1 else 0
+            "<=" -> if (n <= m) 1 else 0
+            ">" -> if (n > m) 1 else 0
+            "<" -> if (n < m) 1 else 0
+            else -> 0
         }
-        
-        if (ineq == ">") {
-            return if (n > m) 1 else 0
-        }
-        
-        return if (n < m) 1 else 0
     }
 }
