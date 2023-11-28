30ed08339e3b2e776abc73985c85863bf14fe189
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-28 23:27:25 +0900

    [level unrated] Title: 이어 붙인 수, Time: 12.65 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-        return (
-            num_list.map { it -> if (it % 2 == 1) it else "" }.joinToString("").toInt() 
-            + num_list.map { it -> if (it % 2 == 0) it else "" }.joinToString("").toInt()
-        )
+        val odd = num_list.map { it -> if (it % 2 == 1) it else "" }.joinToString("").toInt()
+        val even = num_list.map { it -> if (it % 2 == 0) it else "" }.joinToString("").toInt()
+        
+        return odd + even
     }
 }
