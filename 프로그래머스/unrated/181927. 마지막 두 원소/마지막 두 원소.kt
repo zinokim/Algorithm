1f5cedf6975b71b9427e251a0b1f2bc938a0f47c
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-29 23:19:24 +0900

    [level unrated] Title: 마지막 두 원소, Time: 13.59 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.kt
@@ -1,10 +1,10 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = num_list.copyOf(num_list.size + 1)
+        var answer: IntArray = num_list.copyOf()
         val a = num_list.last()
         val b = num_list[num_list.size - 2]
         
-        answer[num_list.size] = (if (a > b) a - b else a * 2)
+        answer += (if (a > b) a - b else a * 2)
         
         return answer
     }
