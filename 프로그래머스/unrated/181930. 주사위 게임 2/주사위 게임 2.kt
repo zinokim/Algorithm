58cccaeaa7d0b5665bdcc463f2104f053c1dbce8
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-29 22:07:30 +0900

    [level unrated] Title: 주사위 게임 2, Time: 25.16 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.kt
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.kt
@@ -1,11 +1,12 @@
 class Solution {
     fun solution(a: Int, b: Int, c: Int): Int {
-        if (a != b && b != c && a != c)
-            return a + b + c
+        val set: Set<Int> = setOf(a, b, c)
         
-        if (a == b && b == c)
-            return (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c))
-        
-        return (a + b + c) * ((a * a) + (b * b) + (c * c)) 
+        return when (set.size) {
+            3 -> a + b + c
+            2 -> (a + b + c) * ((a * a) + (b * b) + (c * c))
+            1 -> (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c))
+            else -> 0
+        }
     }
 }
