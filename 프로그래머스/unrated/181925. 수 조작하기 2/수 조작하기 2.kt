534d00720266b19d19b06741a1759cd87eafa022
kimzino <63464393+zinokim@users.noreply.github.com>
2023-11-30 22:59:02 +0900

    [level unrated] Title: 수 조작하기 2, Time: 41.39 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.kt
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.kt
@@ -1,18 +1,13 @@
 class Solution {
     fun solution(numLog: IntArray): String {
-        var answer: String = ""
-        
-        for (i in 0..numLog.size) {
-            if (i == numLog.lastIndex) break
-            
-            when (numLog[i + 1] - numLog[i]) {
-                1 -> answer += "w"
-                -1 -> answer += "s"
-                10 -> answer += "d"
-                -10 -> answer += "a"
+        return (1..numLog.lastIndex).map {
+            when (numLog[it] - numLog[it - 1]) {
+                1 -> "w"
+                -1 -> "s"
+                10 -> "d"
+                -10 -> "a"
+                else -> ""
             }
-        }
-        
-        return answer
+        }.joinToString("")
     }
 }
