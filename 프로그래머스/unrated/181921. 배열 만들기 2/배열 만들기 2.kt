3b00e7f2f0ee885fa29bd9ff1e7981075f154df3
kimzino <63464393+zinokim@users.noreply.github.com>
2023-12-03 23:27:33 +0900

    [level unrated] Title: 배열 만들기 2, Time: 18.86 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.kt
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.kt
@@ -1,26 +1,20 @@
 class Solution {
+    private val numList: MutableList<Int> = mutableListOf()
+
     fun solution(l: Int, r: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-        
-        for (i in l..r) {
-            if (i % 5 != 0 || !i.toString().startsWith("5")) continue
-            
-            var isTrue: Boolean = true
-            for (c in i.toString().toCharArray()) {
-                if (c != '0' && c != '5') {
-                    isTrue = false
-                    break
-                }
-            }
-            
-            if (isTrue) {
-                answer += i
-                isTrue = true
-            }
-        }
-        
-        if (answer.size == 0) answer += -1
-        
-        return answer
+        dfs(0, "5")
+
+        val result = numList.sorted().filter { it in l..r }.toIntArray()
+
+        return if (result.isEmpty()) intArrayOf(-1) else result
+    }
+
+    private fun dfs(depth: Int, num: String) {
+        if (depth == 6) return
+
+        numList.add(num.toInt())
+
+        dfs(depth + 1, num + "0")
+        dfs(depth + 1, num + "5")
     }
 }
