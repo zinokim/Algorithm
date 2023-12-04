c771ce115758632250f10b7430d0ddcb1b215629
kimzino <63464393+zinokim@users.noreply.github.com>
2023-12-04 18:03:10 +0900

    [level unrated] Title: 배열 만들기 1, Time: 9.92 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.kt
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.kt
@@ -1,3 +1,11 @@
 class Solution {
-    fun solution(n: Int, k: Int) = (k..n step k).toList()
+    fun solution(n: Int, k: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        for (i in k..n step k) {
+            answer += i
+        }
+        
+        return answer
+    }
 }
