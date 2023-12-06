e1c909ab8c7f741dce2b7e64e3578800464498e4
kimzino <63464393+zinokim@users.noreply.github.com>
2023-12-06 22:04:25 +0900

    [level unrated] Title: 배열 만들기 5, Time: 27.79 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.kt
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.kt
@@ -1,10 +1,7 @@
 class Solution {
     fun solution(intStrs: Array<String>, k: Int, s: Int, l: Int): IntArray {
-        var answer: IntArray = intArrayOf()
-        
-        answer += intStrs.map { it -> it.substring(s until s + l).toInt() }
+        return intStrs.map { it -> it.substring(s until s + l).toInt() }
             .filter { it > k }
-        
-        return answer
+            .toIntArray()
     }
 }
