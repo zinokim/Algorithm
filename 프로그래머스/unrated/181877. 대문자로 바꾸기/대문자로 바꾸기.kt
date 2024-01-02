dfcb37af71c374771d98743aa0146f44dfa4e6d7
kimzino <63464393+zinokim@users.noreply.github.com>
2024-01-02 20:21:20 +0900

    [level unrated] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.kt
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(myString: String): String {
-        var answer: String = ""
-        
-        for (i in myString) {
-            answer += i.toUpperCase()
-        }
-        
-        return answer
+        return myString.uppercase()
     }
 }
