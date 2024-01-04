2a96c48598f1b209683e2cb4c986af5a188846be
kimzino <63464393+zinokim@users.noreply.github.com>
2024-01-04 23:35:45 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-        for (i in 0 until num_list.size) {
-            if (num_list[i] < 0) {
-                return i
-            }
-        }
-        
-        return -1
+        return num_list.indexOfFirst { it < 0 }
     }
 }
