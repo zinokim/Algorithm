4a6fbf31e904859090847c938e3097a71671e317
kimzino <63464393+zinokim@users.noreply.github.com>
2024-01-09 22:11:49 +0900

    [level unrated] Title: 리스트 자르기, Time: 11.86 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.kt
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.kt
@@ -1,15 +1,14 @@
 class Solution {
     fun solution(n: Int, slicer: IntArray, num_list: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
         val a: Int = slicer[0]
         val b: Int = slicer[1]
         val c: Int = slicer[2]
         
         return when (n) {
-            1 -> num_list.copyOfRange(0, (b + 1))
-            2 -> num_list.copyOfRange(a, num_list.size)
-            3 -> num_list.copyOfRange(a, (b + 1))
-            else -> num_list.copyOfRange(a, (b + 1)).filterIndexed { index, num -> index % c == 0 }.toIntArray()
-        }
+            1 -> 0..b
+            2 -> a..num_list.lastIndex
+            3 -> a..b
+            else -> a..b step c
+        }.map { num_list[it] }.toIntArray()
     }
 }
