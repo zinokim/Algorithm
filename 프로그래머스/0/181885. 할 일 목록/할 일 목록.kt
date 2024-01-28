058b3e0b043d09710c0d45b703a72e9c22ee66a3
kimzino <63464393+zinokim@users.noreply.github.com>
2024-01-28 23:09:20 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun solution(todos: Array<String>, finished: BooleanArray): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-        
-        for (i in 0 until todos.size) {
-            if (!finished[i]) {
-                answer += todos[i]
-            }
-        }
-        
-        return answer
+        return todos.filterIndexed { i, v -> !finished[i] }.toTypedArray()
     }
 }
