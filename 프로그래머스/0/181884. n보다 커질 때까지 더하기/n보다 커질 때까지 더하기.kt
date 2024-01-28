8da98a19e3a31d022c072a6fa8dc46e07c479c09
kimzino <63464393+zinokim@users.noreply.github.com>
2024-01-28 23:18:40 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(numbers: IntArray, n: Int): Int {
+        var answer: Int = 0
+        
+        for (number in numbers) {
+            answer += number
+            
+            if (answer > n) {
+                return answer
+            }
+        }
+        
+        return answer
+    }
+}
