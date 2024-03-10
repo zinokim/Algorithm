914eccfc480e2e73df4a37cfdcf8325119eb4fd0
kimzino <63464393+zinokim@users.noreply.github.com>
2024-03-10 10:22:41 +0900

    [level 0] Title: x 사이의 개수, Time: 17.48 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.kt
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.kt
@@ -1,12 +1,5 @@
 class Solution {
     fun solution(myString: String): IntArray {
-        var answer: IntArray = intArrayOf()
-        
-        val arr: List<String> = myString.split("x")
-        for (i in 0 until arr.size) {
-            answer = answer.plus(arr[i].length)
-        }
-        
-        return answer
+        return myString.split("x").map { it.length }.toIntArray()
     }
 }
